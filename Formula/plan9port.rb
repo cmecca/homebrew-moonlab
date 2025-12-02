a703aa013dded816318b3631345db3cbeca92f90
Chris Mecca <3637003+cmecca@users.noreply.github.com>
2025-12-01 20:17:47 -0500

    changing bin name

--- Formula/plan9port.rb
+++ Formula/plan9port.rb
@@ -1,9 +1,9 @@
 class Plan9port < Formula
-  desc "Plan 9 from User Space - macOS/arm64 native port by Moonlab"
+  desc "Plan 9 from User Space - macOS native port by Moonlab"
   homepage "https://moonlab.org"
   url "https://github.com/9fans/plan9port/archive/refs/heads/master.tar.gz"
-  version "0.1.0"
   sha256 "814a1aa814d49b6e1a64a3ade3f5ada1496338c30e977ebe8c60cd2e84e3ef06"
+  version "0.1.0"
   license "LPL-1.02"
 
   depends_on :macos
@@ -14,32 +14,28 @@ class Plan9port < Formula
     
     system "./INSTALL", "-b"
     
-    # Install everything
+    # Move bin to plan9bin to avoid conflicts
+    mv "bin", "plan9bin"
+    
+    # Install everything to prefix
     prefix.install Dir["*"]
     
-    # Rename the bin directory to lib
-    (prefix/"lib").mkpath
-    mv prefix/"bin", prefix/"lib/bin"
-    
-    # Create new bin with wrappers
-    bin.mkpath
-    
+    # Create wrapper scripts in the Homebrew bin
     (bin/"9").write <<~EOS
       #!/bin/bash
       export PLAN9=#{prefix}
-      exec #{prefix}/lib/bin/9 "$@"
+      exec #{prefix}/plan9bin/9 "$@"
     EOS
+    chmod 0755, bin/"9"
     
     %w[acme sam 9term].each do |app|
       (bin/app).write <<~EOS
         #!/bin/bash
         export PLAN9=#{prefix}
-        exec #{prefix}/lib/bin/#{app} "$@"
+        exec #{prefix}/plan9bin/#{app} "$@"
       EOS
       chmod 0755, bin/app
     end
-    
-    chmod 0755, bin/"9"
   end
 
   def caveats
